4fda9b0349ee876e017f99fd4b1986abf0c583c5
fMoreira <fabianomoreira.net@gmail.com.br>
2022-11-16 21:48:32 -0300

    banco de dados

--- _db/comandos.sql
+++ _db/comandos.sql
@@ -1,3 +1,4 @@
+/* DDL: linguagem de definição de dados */ 
 create database lojaweb;
 
 use lojaweb;
@@ -20,6 +21,44 @@ show tables;
 
 desc usuario;
 
-INSERT into usuario (nome, email, senha) VALUES('Ana Maria', 'ana@email.com', '12345');
+alter TABLE usuario add data_nasc date null;
+
+CREATE TABLE Funcionario( 
+    id_funcionario int AUTO_INCREMENT PRIMARY KEY,
+    cargo varchar(50) NOT null,
+    id_usuario int NOT Null,
+    FOREIGN KEY(id_usuario) REFERENCES usuario(id_usuario)
+);
+
+
+/* DML: limguagem de manipulação de dados */
+INSERT into usuario (nome, email, senha) 
+VALUES ('Ana Maria', 'ana@email.com', '12345');
+
+INSERT into usuario (nome, email, senha) 
+VALUES
+('Carlos Cunha', 'carlos@email.com', '333333'),
+('Paulo Silva', 'paulo@email.com', '222222'),
+('Maria da Silva', 'maria@email.com', '44444');
+
+INSERT INTO funcionario (id_usuario, cargo) 
+VALUES (1,"Gerente");
 
 select * from usuario;
+
+select usuario.nome, funcionario.cargo 
+from usuario, funcionario;
+
+select usuario.*, funcionario.cargo 
+from usuario, funcionario
+WHERE funcionario.cargo = "Gerente";
+
+SELECT * 
+FROM usuario,funcionario 
+where usuario.id_usuario = funcionario.id_usuario
+and usuario.data_nasc ='2022-12-01';
+
+SELECT * 
+FROM usuario,funcionario 
+where usuario.id_usuario = funcionario.id_usuario 
+and usuario.data_nasc BETWEEN '2022-12-01' and '2022-12-30';
