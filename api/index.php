1c75a1ad3ce4780b1209074c9c77cda47d075c85
fMoreira <fabianomoreira.net@gmail.com.br>
2022-11-18 22:09:20 -0300

    dao

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 include_once("./config/config.php");
+include_once("./model/Usuario.php");
 
 $method = $_SERVER["REQUEST_METHOD"];
 $router = $_SERVER["PATH_INFO"];
@@ -8,9 +9,18 @@ var_dump($_SERVER);
 
 if($method == "POST"){
     if ($router == "/usuario/add"){
-        echo "Cadastro do usuario!";
-        $dadosEntrada = json_decode(file_get_contents('php://input'));
-        var_dump($dadosEntrada);
+        try {     
+            $dadosEntrada = json_decode(file_get_contents('php://input'));
+            var_dump($dadosEntrada);
+            $user = new Usuario();
+            $user->nome = $dadosEntrada->nome;
+            $user->email = $dadosEntrada->email;
+            $user->senha = $dadosEntrada->senha;
+            $user->add();        
+            echo "Usuario Cadastrado!";
+        } catch (Exception $e) {
+            echo "Erro:" . $e->getMessage();
+        }
     }
 }
 
--- api/model/Usuario.php
+++ api/model/Usuario.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("./service/DAO.php");
+
     class Usuario{
         public $id_usuario;//PK - primary key (int)
         public $nome;
@@ -11,7 +13,23 @@
         public $ative = true;
 
         function add(){
-            return "Cadastrado";
+            try{
+                $dao = new DAO;
+                $conn = $dao->conecta();//conectar ao banco de dados;
+                $sql = "INSERT into usuario (nome, foto, cpf, email, senha, telefone) 
+                VALUES (:nome, :foto, :cpf, :email, :senha, :telefone)";//Criar o comando SQL com os paramentos
+                $stman = $conn->prepare($sql);//Prepara o comando SQL para executar;
+                $stman->bindParam(":nome", $this->nome);
+                $stman->bindParam(":email", $this->email);
+                $stman->bindParam(":cpf", $this->cpf);
+                $stman->bindParam(":foto", $this->foto);
+                $stman->bindParam(":senha", $this->senha);
+                $stman->bindParam(":telefone", $this->telefone);
+                $stman->execute();//grava dos dados no banco de dados;
+            }catch(Exception $e){
+                //log
+                throw new Exception("Erro ao cadastra o usuario! ".$e->getMessage());
+            }
         }
     }
 ?>
