2cb5175fcb25a675bd3f20263b63d9fd9f89f3e1
fMoreira <fabianomoreira.net@gmail.com.br>
2022-11-18 21:18:11 -0300

    banco de dados

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
 include_once("./config/config.php");
+
+$method = $_SERVER["REQUEST_METHOD"];
+$router = $_SERVER["PATH_INFO"];
+
+var_dump($_SERVER);
+
+if($method == "POST"){
+    if ($router == "/usuario/add"){
+        echo "Cadastro do usuario!";
+        $dadosEntrada = json_decode(file_get_contents('php://input'));
+        var_dump($dadosEntrada);
+    }
+}
+
