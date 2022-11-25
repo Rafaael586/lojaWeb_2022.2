83fdc7cb88c76f110ad2560dc86a7ad057beb7a4
fMoreira <fabianomoreira.net@gmail.com.br>
2022-11-25 20:54:35 -0300

    form. cadastro user

--- api/controller/usuario.php
+++ api/controller/usuario.php
@@ -1,16 +1,46 @@
 <?php
-include "../model/Usuario.php";
 
-$request = $_REQUEST; //$_GET e $_POST = $_REQUEST
+include_once("./model/Usuario.php");
+function usuarioController($method, $router)
+{
+    if ($method == "POST") {
+        if ($router == "/usuario/add") {
+            try {
+                $dadosEntrada = json_decode(file_get_contents('php://input'));
+                $user = new Usuario();
+                $user->nome = $dadosEntrada->nome;
+                $user->email = $dadosEntrada->email;
+                $user->senha = $dadosEntrada->senha;
+                $user->add();
+                echo "Usuario Cadastrado!";
+            } catch (Exception $e) {
+                echo json_encode("error:" . $e->getMessage());
+            }
+        }
+    }
 
-if (isset($request["add"])){ //isset = verifica de a variavel foi criada ou existe 
-    echo "Cadastro";
-}
-
-if (isset($request["edit"])){
-    echo "Editar";
-}
-
-if (isset($request["list"])){
-    echo "Lista";
+    if ($method == "GET") {
+        if ($router == "/usuario/list") {
+            try {
+                $user = new Usuario();
+                $result = $user->getAll();
+                echo json_encode($result);
+            } catch (Exception $e) {
+                echo json_encode("error:" . $e->getMessage());
+            }
+        }
+        if (!empty(strstr($router, "/usuario/get"))) {
+            try {
+                //$id = $_GET["id"];
+                //var_dump($_GET);
+                //var_dump(explode("/", $router)); //explode: separa uma string em vetor com base em um caracter. No exemplo o "/"
+                $id = explode("/", $router)[3];
+                $user = new Usuario();
+                $result = $user->get($id);
+                echo json_encode($result);
+            } catch (Exception $e) {
+                echo json_encode("error:" . $e->getMessage());
+            }
+        }
+    }
 }
--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 include_once("./config/config.php");
-include_once("./model/Usuario.php");
+include_once("./controller/usuario.php");
 
 $method = $_SERVER["REQUEST_METHOD"];
 $router = $_SERVER["PATH_INFO"];
@@ -14,44 +14,4 @@ header("Access-Control-Allow-Credentials: true");
 
 var_dump($_SERVER);
 
-if($method == "POST"){
-    if ($router == "/usuario/add") {
-        try {
-            $dadosEntrada = json_decode(file_get_contents('php://input'));
-            $user = new Usuario();
-            $user->nome = $dadosEntrada->nome;
-            $user->email = $dadosEntrada->email;
-            $user->senha = $dadosEntrada->senha;
-            $user->add();
-            echo "Usuario Cadastrado!";
-        } catch (Exception $e) {
-            echo json_encode("error:" . $e->getMessage());
-        }
-    }
-}
-
-if($method == "GET"){
-    if ($router == "/usuario/list"){
-        try {     
-            $user = new Usuario();
-            $result = $user->getAll();
-            echo json_encode($result);
-        } catch (Exception $e) {
-            echo json_encode("error:" . $e->getMessage());
-        }
-    }
-    if (!empty(strstr($router,"/usuario/get"))){
-        try {
-            //$id = $_GET["id"];
-            //var_dump($_GET);
-            //var_dump(explode("/", $router)); 
-            $id = explode("/", $router)[3];
-            $user = new Usuario();
-            $result = $user->get($id);
-            echo json_encode($result);
-        } catch (Exception $e) {
-            echo json_encode("error:" . $e->getMessage());
-        }
-    }
-}
-
+usuarioController($method, $router);
