683be2baa7512ada6056fb5a62ca6b182358b188
fMoreira <fabianomoreira.net@gmail.com.br>
2022-11-28 20:34:10 -0300

    form. cadastro user

--- api/index.php
+++ api/index.php
@@ -2,16 +2,21 @@
 include_once("./config/config.php");
 include_once("./controller/usuario.php");
 
-$method = $_SERVER["REQUEST_METHOD"];
-$router = $_SERVER["PATH_INFO"];
+if (isset($_SERVER["REQUEST_METHOD"]) && isset($_SERVER["PATH_INFO"])) {
+    $method = $_SERVER["REQUEST_METHOD"];
+    $router = $_SERVER["PATH_INFO"];
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Max-Age: 3600"); //1hora == 3600 seg;
-header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Max-Age: 3600"); //1hora == 3600 seg;
+    header("Access-Control-Allow-Credentials: true");
 
-//var_dump($_SERVER);
+    //var_dump($_SERVER);
 
-usuarioController($method, $router);
+    usuarioController($method, $router);
+} else {
+    http_response_code(404);
+    echo json_encode(array("message" => "Item does not exist."));
+}
