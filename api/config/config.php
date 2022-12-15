370f21ede3bb567d5a1b2ec9dac872ce8b64dcba
Fabiano Moreira <fabianomoreira.net@gmail.com>
2022-12-14 22:21:16 -0300

    ajustes APi e Portal

--- api/config/config.php
+++ api/config/config.php
@@ -7,11 +7,11 @@ ini_set("display_errors", 1);
 ini_set('display_startup_errors', 1);
 ini_set("enable_post_data_reading", 1);
 
+define("MIDIAS_USER", "/../midias/user"); // Local da pasta onde será gravado os dados
+define("MIDIAS_PRODUTOS", "/../midias/produtos"); // Local da pasta onde será gravado os dados
+
 define("DRIVE", "mysql"); // Qual tipo de banco de dados
 define("HOST", "localhost"); // url ou ip do local do banco de dados
 define("PORT", "3306"); // Porta de acesso ao banco de dados
 define("USER", "root"); // Usuario do banco de dados
 define("DB", "lojaweb"); // Nome do banco de dados
-
-define("MIDIAS_USER", "/../midias/user"); // Local da pasta onde será gravado os dados
-define("MIDIAS_PRODUTOS", "/../midias/produtos"); // Local da pasta onde será gravado os dados
